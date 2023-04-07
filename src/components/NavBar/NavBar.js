aaacc0e63ec8e379d3212b2384cea911a2c65316
JuanAndres <115953281+otromechudo@users.noreply.github.com>
2023-04-07 17:16:51 -0500

    home page

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+"use client";
+import styles from "./NavBar.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function NavBar() {
+  const pathname = usePathname();
+  return (
+    <div className={styles.box}>
+      <li className={styles.list}>
+        <Link href={"/"} className={pathname==="/" ? styles.active :styles.Link}> 
+          <h2>
+            Inicio
+          </h2>
+        </Link>
+        <Link href={"/about"} className={pathname==="/about" ? styles.active :styles.Link}>
+          <h2>Sobre mi</h2>
+        </Link>
+        <Link href={"/skills"} className={pathname==="/skills" ? styles.active :styles.Link}>
+          <h2>Habilidades</h2>
+        </Link>
+        <Link href={"/projects"} className={pathname==="/projects" ? styles.active :styles.Link}>
+          <h2>Projectos</h2>
+        </Link>
+        <Link href={"/contact"} className={pathname==="/contact" ? styles.active :styles.Link}>
+          <h2>Contacto</h2>
+        </Link>
+      </li>
+    </div>
+  );
+}
