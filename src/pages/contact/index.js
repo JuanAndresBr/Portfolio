c4201d8037e734139abae5925d0a8cd3dc3d6baa
Juan Andres <bravobenitezjuanandres@gmail.com>
2023-04-10 23:55:43 -0500

    responsive 80%

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -4,7 +4,27 @@ import { TiLocation } from "react-icons/ti";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { MdAlternateEmail } from "react-icons/md";
 import { useState } from "react";
-import { testInputs } from "./validation";
+
+function testInputs(inputs) {
+  const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  var errors = {};
+  if (inputs.name.length > 30) {
+    errors.name = "Nombre debe ser menor a 30 caracteres";
+  } else if (!inputs.name) {
+    errors.name = "Por favor ingrese el nombre";
+  } else if (inputs.email.length > 50) {
+    errors.email = "El email debe ser menor de 50 caracteres";
+  } else if (!inputs.email) {
+    errors.email = "Por favor ingrese el email";
+  } else if (!regexEmail.test(inputs.email)) {
+    errors.email = "Ingrese un email correcto por favor";
+  } else if (inputs.message.length > 500) {
+    errors.message = "El email debe ser menor de 500 caracteres";
+  } else if (!inputs.message) {
+    errors.message = "Por favor ingrese el mensaje";
+  }
+  return errors;
+}
 
 export default function Contact() {
   const [errors, setErrors] = useState({ name: "", email: "", message: "" });
